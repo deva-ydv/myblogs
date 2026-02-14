5a77266ce76bb907236da2a93598eb8e86e3af8a
Deva <devaydv18@gmail.com>
2026-02-14 23:55:21 +0530

    listBlog

--- client/src/pages/admin/ListBlog.jsx
+++ client/src/pages/admin/ListBlog.jsx
@@ -1,12 +1,30 @@
 import React, { useEffect, useState } from 'react'
-import { blog_data } from '../../assets/assets'
 import BlogTableItem from '../../components/admin/BlogTableItem'
+import { useAppContext } from '../../context/AppContext'
+import toast from 'react-hot-toast'
 
 const ListBlog = () => {
   const [blogs, setBlogs] = useState([])
+  const {axios} = useAppContext()
   const fetchBlogs = async () =>{
-    setBlogs(blog_data)
+  try {
+    const { data } = await axios.get('/api/admin/blogs');
+
+    if (data.success) {
+      setBlogs([...data.blogs]);   // IMPORTANT FIX
+    } else {
+      toast.error(data.message);
+    }
+
+  } catch (error) {
+    if (error.response) {
+          toast.error(error.response.data.message);
+      } else {
+          toast.error(error.message);
+      }
   }
+};
+
   useEffect(()=>{
     fetchBlogs()
   },[])
